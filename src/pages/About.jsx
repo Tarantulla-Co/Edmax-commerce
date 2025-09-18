8e392d15549994dc2c0f8b9038f5532686dd69d3
ferguson jezreel <anojafa24@gmail.com>
2025-09-18 16:06:25 +0000

    play

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Swal from 'sweetalert2';
 
 export default function About() {
+    const msg = ()=>{
+    Swal.fire({
+    title: 'You Clicked Me',
+    icon:'warning',
+    text: 'Bro you clicked me'
+    })
+    }
   return (
     <div className="container mt-5">
       <h1 className="text-primary">Hello Bootstrap + React 🚀</h1>
-      <button className="btn btn-success">Click Me</button>
+      <button className="btn btn-success" onClick={msg}>Click Me</button>
     </div>
   );
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
+import Swal from 'sweetalert2'
 export default function Home() {
+Swal.fire('Welcome', 'Edmax Setup', 'success');
   return (
     <div>
       <h1 className='text-center'>Hello Edmax</h1>
